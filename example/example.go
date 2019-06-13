ee82881d8ce59dfd0ad75ca7e2f7943b522d28dd
fabarj4 <fabarj4@gmail.com>
2019-06-13 09:52:51 +0700

    sekarang main di ubah menjadi contoh

---
+++ example/example.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/fabarj4/goroomchat"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		content, err := ioutil.ReadFile("../index.html")
+		if err != nil {
+			http.Error(w, "Could not open requested file", http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, "%s", content)
+	})
+
+	//fungsi ini digunakan untuk melakukan chat
+	http.HandleFunc("/ws", goroomchat.HandleWS)
+
+	//fungsi ini untuk menampilkan semua room yang sedang digunakan
+	http.HandleFunc("/rooms/", goroomchat.HandleRooms)
+
+	fmt.Println("Server starting at :8080")
+	http.ListenAndServe(":8080", nil)
+}
