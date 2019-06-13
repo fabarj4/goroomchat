5320a75b18612d5c98d03e1e1f3ead6ead59942c
fabarj4 <fabarj4@gmail.com>
2019-06-13 10:32:55 +0700

    sekarang index.html dianggap contoh

--- example/example.go
+++ example/example.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		content, err := ioutil.ReadFile("../index.html")
+		content, err := ioutil.ReadFile("index.html")
 		if err != nil {
 			http.Error(w, "Could not open requested file", http.StatusInternalServerError)
 			return
